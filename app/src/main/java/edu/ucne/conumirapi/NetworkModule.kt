9711100906d29ef18db9dc0a4de1624676f6bd44
AngelParedes12 <angel_paredes@ucne.edu.do>
2025-10-14 19:55:28 -0400

    Nuevos import y agregando la URL larga por error de que no se lee cuando es corta

--- app/src/main/java/edu/ucne/conumirapi/NetworkModule.kt
+++ app/src/main/java/edu/ucne/conumirapi/NetworkModule.kt
@@ -1,21 +1,35 @@
 package edu.ucne.conumirapi
 
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
+import java.util.concurrent.TimeUnit
 
 object NetworkModule {
-    private const val BASE_URL = "https://prioridades-api-angel.azurewebsites.net/"
+    private const val BASE_URL = "https://prioridades-api-angel-dnbsd7hue7bkbsaq.eastus-01.azurewebsites.net/"
 
     val api: ApiService by lazy {
-        val log = HttpLoggingInterceptor().apply { level = HttpLoggingInterceptor.Level.BODY }
-        val client = OkHttpClient.Builder().addInterceptor(log).build()
+        val logging = HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+        val client = OkHttpClient.Builder()
+            .connectTimeout(15, TimeUnit.SECONDS)
+            .readTimeout(15, TimeUnit.SECONDS)
+            .writeTimeout(15, TimeUnit.SECONDS)
+            .addInterceptor(logging)
+            .build()
+
+        val moshi = Moshi.Builder()
+            .add(KotlinJsonAdapterFactory())
+            .build()
 
         Retrofit.Builder()
             .baseUrl(BASE_URL)
             .client(client)
-            .addConverterFactory(MoshiConverterFactory.create())
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
             .build()
             .create(ApiService::class.java)
     }
